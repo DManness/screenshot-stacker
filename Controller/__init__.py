f82ed0193643adec280f99f851aae0bb6c22c76d
dmanness <29873268+DManness@users.noreply.github.com>
2021-03-26 14:00:34 -0500

    Added function documentation.

--- Controller/__init__.py
+++ Controller/__init__.py
@@ -4,6 +4,15 @@ from Model.ImageThumbItem import ImageThumbItem
 
 
 def smart_crop_image(image_handle):
+    """
+    Crops an image to a square, making the best use of the height and width.
+    The resulting crop will be taken from the center of the image.
+
+    :param image_handle: a reference to a PILlow image.
+    :type image_handle: PIL.Image
+    :return: a cropped PIL.Image
+    """
+
     width, height = image_handle.size
     if height > width:
         adjust = int((height - width) / 2)
@@ -16,6 +25,14 @@ def smart_crop_image(image_handle):
 
 
 def create_thumbnail(image_path, size=64):
+    """
+        creates a square thumbnail from an image.
+        :param image_path: The path on disk where the image file is located.
+        :type image_path: str
+        :param size: The height/width of the resulting thumbnail. (default size is 64)
+        :type size: int
+    """
+
     image = open(image_path, 'rb')
     thumb = smart_crop_image(Image.open(image))
 
@@ -25,6 +42,15 @@ def create_thumbnail(image_path, size=64):
 
 
 def create_thumb_item(image_path, size=64):
+    """
+    Opens an image from disk and wraps it in an ImageThumbItem. This allows it
+    to be used in the gui.
+    :param image_path: The path on disk where the image file is located.
+    :type image_path: str
+    :param size: The height/width of the resulting thumbnail. (default size is 64)
+    :type size: int
+    """
+
     full_path = os.path.abspath(image_path)
     base_name = os.path.basename(full_path)
     thumb = create_thumbnail(full_path, size=size)
@@ -32,12 +58,25 @@ def create_thumb_item(image_path, size=64):
 
 
 def create_composite_image(image_array, orientation='vertical', alignment='left'):
+    """
+    Creates a composite image in which each image is stacked top to bottom
+    or side-by-side. The resulting image is stored in memory.
+    :param image_array:
+    :type image_array: list(str)
+    :param orientation: (default is 'vertical')
+    :type orientation: str
+    :param alignment: (default is 'left')
+    :type alignment: str
+    :return: PIL.Image
+    """
+
     is_vert = orientation == 'vertical'
     out_size_v = 0
     out_size_h = 0
     largest_width = 0
     largest_height = 0
-    # This first round discovers information about each file provided.
+
+    # This first pass discovers information about each file provided.
     for img in image_array:
         with open(img.full_name, 'rb') as file_pointer:
             img_handle = Image.open(file_pointer)
